25ef1a6bb6689db1d31b9578063fd3ed3df935b6
liuchengfei1993 <869275085@qq.com>
2019-03-01 09:28:12 +0800

    10

--- 221_test/app.js
+++ 221_test/app.js
@@ -23,15 +23,15 @@ var connection = mysql.createConnection({
   // port: '8080',
   user: 'root',
   password: 'root',
-  database:'db1',
+  database: 'db1',
 })
 
 connection.connect();
 var sql = "select * from person";
 var str = "";
-connection.query(sql, function (err, result) {
+connection.query(sql, function(err, result) {
   if (err) {
-    console.log("ERROR",err.message);
+    console.log("ERROR", err.message);
   }
   str = JSON.stringify(result);
   //数据库查询的数据保存在result中，但浏览器并不能直接读取result中的结果，因此需要用JSON进行解析
@@ -60,9 +60,22 @@ app.get('/', function(req, res) {
 
 })
 
-var server = app.listen(3389, function() {
+var server = app.listen(3889, function() {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log("host:" + host + ",port:" + port);
 })
+
+const http = require('https');
+
+http.get('https://api.weixin.qq.com/sns/jscode2session?appid=wx43c1c9647ca0209f&secret=67b546c0e5af6496ec5b3b2e747ef25a&js_code=0231HZcG1v1sN80STJbG1Pu6dG11HZcc&grant_type=authorization_code', function(req, res) {
+  let html = '';
+  req.on('data', function(data) {
+    html += data;
+  });
+  req.on('end', function() {
+    let result = JSON.parse(html);
+    console.log(result);
+  });
+});
